ce8db21563904dac567ccdae3adb2fc488ff1e4e
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-11-06 12:29:34 +0100

    password generator added

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
-
-
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+
+    letters_list = [random.choice(letters) for char in range(nr_letters)]
+
+    symbols_list = [random.choice(symbols) for char in range(nr_symbols)]
+
+    numb_list = [random.choice(numbers) for char in range(nr_numbers)]
+
+    password_list = letters_list + symbols_list + numb_list
+    random.shuffle(password_list)
+
+    password = ''.join(password_list)
+    pass_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -54,7 +77,7 @@ pass_entry = Entry(width=32)
 pass_entry.grid(row=3, column=1)
 
 # Button
-generate_pass_button = Button(text='Generate Password', width=14)
+generate_pass_button = Button(text='Generate Password', width=14, command=generate_password)
 generate_pass_button.grid(row=3, column=2)
 add_button = Button(text='Add', width=43, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
